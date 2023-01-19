364baa5e678675338e2c38c231a802e9d3e5de24
XI <khchow@xandi.co.jp>
2023-01-19 16:34:19 +0600

    Feat: Drawer add

--- lib/color_detail_page.dart
+++ lib/color_detail_page.dart
@@ -12,6 +12,19 @@ class ColorDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle textStyle = theme.textTheme.bodyMedium!;
+    final List<Widget> aboutBoxChildren = <Widget>[
+      const SizedBox(height: 24),
+      RichText(
+        text: TextSpan(
+          children: <TextSpan>[
+            TextSpan(style: textStyle, text: "This is my first flutter app "),
+          ],
+        ),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: color,
@@ -19,8 +32,48 @@ class ColorDetailPage extends StatelessWidget {
           '$title[$materialIndex]',
         ),
       ),
-      body: Container(
-        color: color[materialIndex],
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Show About Example'),
+          onPressed: () {
+            showAboutDialog(
+              context: context,
+              applicationName: 'Flutter App',
+              children: aboutBoxChildren,
+            );
+          },
+        ),
       ),
     );
   }
